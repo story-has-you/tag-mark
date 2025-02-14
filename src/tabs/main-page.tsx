7d763c0df510a9efe7ae65ea75937c676c9703f0
fangxi <loveinori@qq.com>
2025-02-14 13:47:22 +0800

    更新主页面组件，添加按钮并引入全局样式；调整 tsconfig.json 以支持新的路径别名

--- src/tabs/main-page.tsx
+++ src/tabs/main-page.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React from "react"
+
+import "@/styles/globle.css"
 
 const MainPage: React.FC = () => {
-  return <>MainPage</>;
-};
+  return (
+    <button
+      type="button"
+      className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      Count:
+    </button>
+  )
+}
 
-MainPage.displayName = "MainPage";
+MainPage.displayName = "MainPage"
 
-export default MainPage;
+export default MainPage
